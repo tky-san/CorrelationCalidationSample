ef757f94601bfa5173e191cafa8348813500f436
tky-san <cist.b214.t.ito@gmail.com>
2025-11-15 14:45:44 +0900

    CorrelationCalidationSample
    CorrelationCalidationSample　作成

---
+++ CorrelationCalidationSample/src/main/java/com/example/demo/CorrelationCalidationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrelationCalidationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrelationCalidationSampleApplication.class, args);
+	}
+
+}
